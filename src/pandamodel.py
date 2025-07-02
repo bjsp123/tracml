6ddd510880c8a850ddae38e67ae3a8d657f8a7d2
bjsp123 <hwacha.2@gmail.com>
2025-07-02 14:53:17 +0100

    test schema added

--- src/pandamodel.py
+++ src/pandamodel.py
@@ -19,10 +19,13 @@ class PandaModel(trac.TracModel):
 
     def define_outputs(self) -> tp.Dict[str, trac.ModelOutputSchema]:
         dq_metrics = trac.define_output_table(
-            trac.F("metric1", trac.FLOAT, label="Metric 1"),
-            trac.F("metric2", trac.FLOAT, label="Metric 2"))
+            trac.F("metric_name", trac.STRING, label="Metric Name"),
+            trac.F("metric_value", trac.FLOAT, label="Metric Value"))
+        
+        approved_loans = trac.load_schema(schemas, "loans_schema_kaggle.csv")
 
-        return {"loans_dq_metrics": dq_metrics}
+        return {"loans_dq_metrics": dq_metrics,
+                "approved_loans_clean": trac.ModelOutputSchema(approved_loans)}
 
 
     def run_model(self, ctx: trac.TracContext):
@@ -30,6 +33,17 @@ class PandaModel(trac.TracModel):
         ctx.log().info("Panda model is running")
 
         ctx.log().info(f"This is pandamodel")
+
+        loansdf = ctx.get_pandas_table("approved_loans")
+
+        dqdf = pd.DataFrame({"metric_name":["metric a", "metric b"], "metric_value":[0.9,0.93]})
+
+        #right, first output some dummy DQ metrics
+        ctx.put_pandas_table("loans_dq_metrics", dqdf)
+
+        #now output the 'cleaned' data but don't do any actual cleaning as such
+        ctx.put_pandas_table("approved_loans_clean", loansdf)
+
 
 if __name__ == "__main__":
     import tracdap.rt.launch as launch
